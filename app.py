3c89e021e5d73a972c9f4aa5173b768b44af9aec
neemokim <inbloom2007@daum.net>
2025-04-18 21:49:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import datetime
-from open_dart_reader import process_corp_info, get_dart_report_data
 
 st.set_page_config(page_title="DART 재무정보 통합조회기", layout="wide")
 st.title("📊 DART 재무정보 통합조회기")
